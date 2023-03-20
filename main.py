bdb8b5b38ce32f7765fe78a5c83a992b2d91e60c
Bibekkun <bibekrai655@gmail.com>
2023-03-20 13:41:38 +0545

    added permalink and write top post html file

--- main.py
+++ main.py
@@ -17,30 +17,34 @@ def top_post_stats(subreddit_name, time_filter):
                         client_secret='NFoNppi9lNNZZgGuyiZpdvpVAc5_5A',
                         user_agent=user_agent)
 
-    # Retrieve the top post by upvotes
     top_by_upvotes = None
     for submission in reddit.subreddit(subreddit_name).top(limit=1):
         if submission.score > 0:
             top_by_upvotes = submission
             break
 
-    # Print the top post by upvotes
     if top_by_upvotes is not None:
-        print('Top post by upvotes: "{}" with {} upvotes'.format(top_by_upvotes.title, top_by_upvotes.score,top_by_upvotes.permalink))
+        print('Top post by upvotes: "{}" with {} upvotes ({})'.format(top_by_upvotes.title, top_by_upvotes.score,top_by_upvotes.permalink))
     else:
         print('No posts found that meet the search criteria.')
 
-    # Retrieve the top post by number of comments
     top_by_comments = None
-    for submission in reddit.subreddit(subreddit_name).top('day', limit=1):
+    for submission in reddit.subreddit(subreddit_name).top(time_filter='day', limit=1):
         if submission.num_comments > 0:
             top_by_comments = submission
             break
 
-    # Print the top post by number of comments
-    if top_by_comments is not None:
-        print('Top post by comments: "{}" with {} comments'.format(top_by_comments.title, top_by_comments.num_comments,top_by_comments.permalink))
+    if top_by_upvotes is not None:
+        print('Top post by comments: "{}" with {} comments ({})'.format(top_by_comments.title, top_by_comments.num_comments,top_by_comments.permalink))
     else:
         print('No posts found that meet the search criteria.')
+
+    with open('top_posts.html', 'w') as f:
+        f.write('<html>\n<head>\n<title>Top Posts on r/{}</title>\n</head>\n<body>\n'.format(subreddit_name))
+        if top_by_upvotes is not None:
+            f.write('<p>Top post by upvotes: <a href="{}">{}</a> with {} upvotes</p>\n'.format(top_by_upvotes.permalink, top_by_upvotes.title, top_by_upvotes.score))
+        if top_by_comments is not None:
+            f.write('<p>Top post by comments: <a href="{}">{}</a> with {} comments</p>\n'.format(top_by_comments.permalink, top_by_comments.title, top_by_comments.num_comments))
+        f.write('</body>\n</html>')
 
 top_post_stats('Nepal', time_filter='day')
